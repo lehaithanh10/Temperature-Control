ae049d501a40b4e58008a3fd60ea2907b110bc48
lehaithanh1510 <haithuthanhtien@gmail.com>
2023-03-01 09:41:35 +0700

    fix: update mqtt topic name

--- src/modules/utils/mqtt/types.ts
+++ src/modules/utils/mqtt/types.ts
@@ -1,6 +1,6 @@
 export enum MQTTTopic {
   NEW_DEVICE = "new/device",
-  GET_MEASURE_DATA = "measure_data/sensor/#",
+  GET_MEASURE_DATA = "get_measure_data/sensor/#",
   LIGHT_BULB_ACTION = "device/light-bulb",
   WARNING_USER = "warning/user",
 }
